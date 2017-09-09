e0404a58274ab97832b704cb3a05a5f9d0d41ec5
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-09-10 00:34:55 +0100

    added subtraction

--- main.js
+++ main.js
@@ -9,3 +9,10 @@ if (calculator === "a") {
 
 }
 
+else if (calculator === "s") {
+	var num1 = prompt("Please enter your first number.");
+	var num2 = prompt("Please enter your second number.");
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log(num1 - num2);
+}
