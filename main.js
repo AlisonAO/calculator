c08207689bc8e4d15d6be571096a9cf6fbb103ed
AlisonAO <alison_opeloyeru@hotmail.co.uk>
2017-09-10 00:20:32 +0100

    added index.html and main.js files

---
+++ main.js
@@ -0,0 +1,51 @@
+var calculator = prompt("Welcome to console calculator. Please select which operator you wish to use", "(a)ddition, (s)ubtraction, (d)ivision, (m)ultiplication, (sq)uare, (p)ower");
+
+if (calculator === "a") {
+	var num1 = prompt("Please enter your first number.");
+	var num2 = prompt("Please enter your second number.");
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log(num1 + num2);
+
+}
+
+else if (calculator === "s") {
+	var num1 = prompt("Please enter your first number.");
+	var num2 = prompt("Please enter your second number.");
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log(num1 - num2);
+}
+
+else if (calculator === "d") {
+	var num1 = prompt("Please enter your first number.");
+	var num2 = prompt("Please enter your second number.");
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log(num1 / num2);
+}
+
+else if (calculator === "m") {
+	var num1 = prompt("Please enter your first number.");
+	var num2 = prompt("Please enter your second number.");
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log(num1 * num2);
+}
+
+else if (calculator === "p") {
+	var num1 = prompt("Please enter your first number.");
+	var num2 = prompt("Please enter your second number.");
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+	console.log(Math.pow(num1, num2)); 
+}
+
+else if (calculator === "sq") {
+	var num1 = prompt("Please enter your number.");
+	console.log(Math.pow(num1, 2));
+}
+
+else {
+	console.log("Sorry, you can't be helped.")
+}
